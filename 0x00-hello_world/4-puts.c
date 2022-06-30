a586303a7f3bdca1491750df2bc6289caa4e83d4
Olayinka <adebisiolayinka30@gmail.com>
2022-06-30 13:57:35 -0700

    Finish tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/**Print a string using "puts"*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
