2441d36b9fd6d0b71c27824e124b91583ed162b6
Olayinka <adebisiolayinka30@gmail.com>
2022-06-30 13:37:12 -0700

    Done with tasks for todar

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	char chr;
+	int num;
+	long lnum;
+	long long llnum;
+	float float_digit;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(chr));
+	printf("Size of an int: %lu byte(s)\n", sizeof(num));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lnum))
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llnum));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float_digit));
+
+	return (0);
+}
