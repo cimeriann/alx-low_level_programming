8ff69e240464d42fd1646ee68ba6b3c9e9deb2d8
Olayinka <adebisiolayinka30@gmail.com>
2022-06-30 14:36:31 -0700

    committing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ *   main - Entry point
  *
- *   *
+ *   
  *
- *    * Return: Always 0 (Success)
+ *   Return: Always 0 (Success)
  *
  *     
  */
