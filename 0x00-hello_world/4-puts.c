0fa323c9130ed1c9d9c30fd08f0f7fa36e2840c3
Olayinka <adebisiolayinka30@gmail.com>
2022-06-30 14:25:18 -0700

    llaala

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,8 @@
  *
  *    * Return: Always 0 (Success)
  *
- *     */
+ *     
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
  *
- *    * Return: Always 0 (Success)
  *
- *     */
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
 	char chr;
