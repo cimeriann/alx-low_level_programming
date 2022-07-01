4e041843eed11e13de330bf26b1acd2bffec9aa0
Olayinka <adebisiolayinka30@gmail.com>
2022-06-30 18:56:18 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  *
  *	main - Entry point
@@ -9,9 +10,10 @@
  *	Return: Always 0 (Success)
  *
  */
+
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
