b6f28c4a5e879b9a5af1185b67e38c80b21bf159
Olayinka <adebisiolayinka30@gmail.com>
2022-07-05 10:01:56 -0700

    Create solution for task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
 int _putchar(char c);
 void print_alphabet(void);
 #endif
--- 0x02-functions_nested_loops/print_alphabet.c
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 void print_alphabet(void)
 {
 	char c = a;
