e189e433f4aa070454f7b032fa4f830735264a29
Olayinka <adebisiolayinka30@gmail.com>
2022-07-07 15:50:02 -0700

    Two more tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - checks for a number
+ * @a: digit to be checked
+ * @b: digit to cbe checked
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int times;
+	
+	times = a * b;
+
+	return (times);
+
+
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
