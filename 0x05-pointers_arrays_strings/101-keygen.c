995825ea8c48ee727c58fb5c7ff12db958e349dd
Olayinka <adebisiolayinka30@gmail.com>
2022-07-12 15:59:55 -0700

    update advanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- genrate password
+ * Return:nothing
+ */
+
+int main(void)
+{
+	int ascii = 2772, i = 0, j, random;
+	char password[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		password[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
+	return (0);
+}
+
