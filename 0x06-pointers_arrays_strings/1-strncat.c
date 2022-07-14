5be56742403294479413c07b68e9f689f63e6d3c
olayinka <adebisiolayinka30@gmail.com>
2022-07-14 12:07:55 -0700

    pushing to github

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[length +i] = src[i];
+		dest[length + i] = src[i];
 	}
 	dest[length + i] = '\0';
 
