3b4e06bfde054ec8df1b7b589715957962780e40
olayinka <adebisiolayinka30@gmail.com>
2022-07-14 20:42:07 -0700

    updating

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncpy - copy a string
@@ -8,7 +7,7 @@
  * @n: n
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
