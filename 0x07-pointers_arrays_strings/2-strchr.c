feb4f929cd6285fe72c1f4d89278de87a09ea89a
olayinka <adebisiolayinka30@gmail.com>
2022-07-18 16:47:33 -0700

    task 3 & 4

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * __strchr - locates a char in a string
+ *@s: string
+ * @c: the char
+ * Return: a pointer to the first occurence or null
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
+	return (s + 1);
+}
