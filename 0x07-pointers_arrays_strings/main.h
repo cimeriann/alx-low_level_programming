4dabb70b12e067669cc59c86666788c34e695a0a
olayinka <adebisiolayinka30@gmail.com>
2022-07-19 00:57:34 -0700

    pushing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -8,5 +5,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
