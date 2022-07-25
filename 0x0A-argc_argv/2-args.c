41e679115ea66113757ed2175287df198d98a922
olayinka <adebisiolayinka30@gmail.com>
2022-07-25 10:21:09 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints num of args passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	int index;
+ 
+	for (index = 0, index < argc; index++)
+	{
+		printf("%s\n", argv[index]);
+	}
+	return (0);
+}
