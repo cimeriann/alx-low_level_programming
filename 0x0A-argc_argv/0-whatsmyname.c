94208b0cef3ce635de13746aa3367f5f139d6a63
olayinka <adebisiolayinka30@gmail.com>
2022-07-25 09:30:46 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <main.h>
 
 /**
  * main - function that prints its name
@@ -11,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	void(argc);
+	(void)argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
