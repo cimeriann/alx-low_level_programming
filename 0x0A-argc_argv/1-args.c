a97ba3e8ec534b75aed8a91e96b3974478a89e0a
olayinka <adebisiolayinka30@gmail.com>
2022-07-25 09:37:33 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
