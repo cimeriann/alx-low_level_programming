803ce743b9e5a6af9837fb70f039b088371a0879
olayinka <adebisiolayinka30@gmail.com>
2022-07-25 11:44:32 -0700

    task 5

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,13 +15,15 @@ int main(int argc, char *argv[])
 	if (argc < 3)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
 
 	a = atoi(argv[1]);
+
 	b = atoi(argv[2]);
 
 	printf("%d\n", a * b);
-	
+
 	return (0);
 }
