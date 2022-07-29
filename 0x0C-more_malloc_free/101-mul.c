d8cf158a65c949b4e4129acec3ea9bc749c1cab1
olayinka <adebisiolayinka30@gmail.com>
2022-07-28 23:19:46 -0700

    task 5 fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,11 +13,11 @@ int _isdigit(int c)
 }
 
 /**
- * * _strlen - returns the length of a string
- * * @s: the string whose length to check
- * *
- * * Return: integer length of string
- * */
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
 int _strlen(char *s)
 {
 	int i = 0;
