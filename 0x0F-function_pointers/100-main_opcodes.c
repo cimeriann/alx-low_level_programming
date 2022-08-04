1cc7ce0368e399485a20338f2d11e352b32648e7
olayinka <adebisiolayinka30@gmail.com>
2022-08-04 08:35:10 -0700

    task 5 fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,26 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, n;
+int i, n;
 
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	n = atoi(argv[1]);
-	if (n < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+exit(2);
+}
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%02hhx", *((char *)main + i));
-		if (i < n - 1)
-			printf(" ");
-		else
-			printf("\n");
-	}
+for (i = 0; i < n; i++)
+{
+printf("%02hhx", *((char *)main + i));
+if (i < n - 1)
+printf(" ");
+else
+printf("\n");
+}
+
+return (0);
+}
