b27fee45efe00989f7e90e3a01b9c218d15cf9bf
olayinka <adebisiolayinka30@gmail.com>
2022-08-04 08:46:17 -0700

    task 3 fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,10 +3,10 @@
 
 /*
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
 	char *op;
@@ -18,5 +18,4 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 #endif
