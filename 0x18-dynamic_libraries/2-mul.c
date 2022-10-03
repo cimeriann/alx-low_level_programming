b658157833fb3991349bbb62ba1fd4b86ef25780
Olayinka <adebisiolayinka30@gmail.com>
2022-10-03 08:35:33 -0700

    task 3 fix

---
+++ 0x18-dynamic_libraries/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - checks for a number
+ * @a: digit to be checked
+ * @b: digit to be checked
+ * Return: the multiplicationof the two int s
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x18-dynamic_libraries/3-sub.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * sub - checks for a number
+ * @a: digit to be checked
+ * @b: digit to be checked
+ * Return: the multiplicationof the two int s
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
